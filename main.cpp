b3703413e38ad3aedfe42c758cdf560398a9c909
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2026-04-21 10:21:37 +0200

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+char* buf;
+
+int sum_to_n(int num) {
+  int i, sum = 0;
+  for (i = 1; i <= num; i++) sum += i;
+  return sum;
+}
+
+void printSum() {
+  char line[10];
+  printf("Enter a number: ");
+  fgets(line, 10, stdin);
+
+  if (line != NULL) strtok(line, "\n");
+
+  sprintf(buf, "sum=%d", sum_to_n(atoi(line)));
+  printf("%s\n", buf);
+}
+
+int main() {
+  printSum();
+  return 0;
+}
